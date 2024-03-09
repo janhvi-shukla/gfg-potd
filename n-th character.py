597e7384752cffaf01c33d1f832bd0de7f87e6d5
Janhvi Shukla <92948405+janhvi-shukla@users.noreply.github.com>
2024-03-09 22:53:56 +0530

    Create n-th character.py

---
+++ n-th character.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nthCharacter(self, s, r, n):
+        # code here
+        corr={
+            '1':'10',
+            '0':'01'
+        }
+        
+        r=1<<r
+        while r>1:
+            index=n//r
+            s=corr[s[index]]
+            if n>=r:
+                n=n%r
+            r=r//2
+        return s[n//r]
